67cd8b387300075c9516a8d3642501149422db88
YohoYang <yohoyang@outlook.com>
2022-11-26 21:06:54 +0800

    修正解流文件不存在时无法恢复默认状态

--- VSGUI/API/DemuxApi.cs
+++ VSGUI/API/DemuxApi.cs
@@ -54,6 +54,7 @@ namespace VSGUI.API
             else
             {
                 MessageBoxApi.Show(LanguageApi.FindRes("fileNotExist"), LanguageApi.FindRes("error"));
+                ExitedCall("");
                 return;
             }
 
