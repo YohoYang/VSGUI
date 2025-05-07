64304b5282275a19390f0755751857d6e27ddb99
YohoYang <yohoyang@outlook.com>
2025-05-07 22:38:24 +0800

    1.0.20修正拖动

--- VSGUI/MainWindow.xaml.cs
+++ VSGUI/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace VSGUI
         public static string binpath = Directory.GetCurrentDirectory() + @"\bin";
         public static string envpath = null;
         private bool forcedStop = false;
-        private string coreversion = "v1.0.19";
+        private string coreversion = "v1.0.20";
         public static string logBoxStr = "";
         private string[] videoMultiInputLists, audioMultiInputLists;
 
